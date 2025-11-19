569ad3e3eb24f908e42c00fadf93f246d26c5cbb
DudaCmps <santcamposeduarda@gmail.com>
2025-11-19 13:37:58 -0300

    armazenando JSON no banco de dados

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -38,6 +38,70 @@
             <textarea name="description" id="description" class="form-control" placeholder="Descreva seu evento!"></textarea>
         </div>
 
+        <div class="form-group mb-3">
+            <label for="items" class="fw-semibold">Adicione itens de infraestrutura:</label>
+           
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Sound System"> Sound System
+            </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Luzes Estroboscópicas"> Luzes Estroboscópicas
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Máquina de Fumaça"> Máquina de Fumaça
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Pista de Dança"> Pista de Dança
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Lounge Chillout"> Lounge Chillout
+              </div>
+
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Painel de LED"> Painel de LED
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Área VIP"> Área VIP
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Food Truck"> Food Truck
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Banheiros Químicos"> Banheiros Químicos
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Iluminação Decorativa"> Iluminação Decorativa
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Backstage"> Backstage
+              </div>
+              
+              <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cercamento do Espaço"> Cercamento do Espaço
+              </div>
+           
+           <div class="form-group">
+            <input type="checkbox" name="items[]" value="Palco"> Palco
+           </div>
+
+           <div class="form-group">
+            <input type="checkbox" name="items[]" value="Open Food"> Open Food
+           </div>
+
+           <div class="form-group">
+            <input type="checkbox" name="items[]" value="Open Bar"> Open Bar
+           </div>
+        </div>
+
         <input type="submit" class="btn btn-dark w-100 mt-3" value="Criar Evento">
     </form>
 </div>
