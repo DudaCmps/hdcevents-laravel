4f02b3e6d2bd1843721d347a835780ad322fa990
DudaCmps <santcamposeduarda@gmail.com>
2025-11-24 17:25:03 -0300

    Contagem de participantes

--- hdcevents/resources/views/events/dashboard.blade.php
+++ hdcevents/resources/views/events/dashboard.blade.php
@@ -25,7 +25,8 @@
                     <td scope="row">{{$loop->index + 1}}</td>
                     <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
 
-                    <td scope="row">0</td>
+                    <td scope="row"> {{ count($event->users) }} </td>
+
                     <td scope="row">
                         <a href="/events/edit/{{ $event->id }}" class="btn btn-info btn-sm"> <ion-icon name="create-outline"></ion-icon> Editar</a> 
                         <form action="/events/{{ $event->id}}" method="POST">
@@ -42,6 +43,42 @@
     </table>
     @else
         <p>Você ainda não tem eventos, <a href="/events/create">criar evento</a></p>
+    @endif
+</div>
+
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Eventos que participo</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
+    @if(count($eventsasparticipant) > 0);
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Participantes</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($eventsasparticipant as $event)
+                    <tr>
+                        <td scope="row">{{$loop->index + 1}}</td>
+                        <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
+
+                        <td scope="row"> {{ count($event->users) }} </td>
+
+                        <td scope="row">
+                            <a href="#" class="btn btn-outline-danger btn-sm">Cancelar participação</a>
+                        </td>
+                    </tr>
+
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Você ainda não participa de nenhum evento</p>
     @endif
 </div>
 
